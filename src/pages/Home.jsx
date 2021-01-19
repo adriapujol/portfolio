4e4661b2c62515d986d37b5c3a758ccac84690ae
Adria Pujol <adriapujolaparicio@gmail.com>
2021-01-19 19:38:21 +0100

    image drawing import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import drawing from '../img/meDrawing.png';
 
 const Home = () => {
     return (
         <div className="content-wrapper">
             <div className="hero">
                 HELLO THIS IS THE HOME
+                {/* <img src={drawing} alt="Adria Pujol" /> */}
             </div>
         </div>
     )
