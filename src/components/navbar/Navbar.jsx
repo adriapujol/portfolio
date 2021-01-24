e5ceba441af114b7917b2804bf51a6662cd2f673
Adria Pujol <adriapujolaparicio@gmail.com>
2021-01-24 19:52:37 +0100

    navbar kinda working

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import './Navbar.scss';
 const Navbar = () => {
 
     const [solidBackground, setSolidBackground] = useState(false);
+    const [clickedBurger, setClickedBurger] = useState(false);
 
     const changeBackground = () => {
         if (window.scrollY > 150) {
@@ -13,6 +14,10 @@ const Navbar = () => {
         }
     }
 
+    const updateBurger = () => {
+        setClickedBurger(burger => setClickedBurger(!burger));
+    }
+
     window.addEventListener('scroll', changeBackground);
 
     return (
@@ -20,10 +25,15 @@ const Navbar = () => {
             <div className="logo">
                 <a className="home-link" href="#home">LOGO</a>
             </div>
-            <div className="nav-links">
-                <a className={solidBackground ? "link link-white" : "link link-black"} href="#about">About</a>
-                <a className={solidBackground ? "link link-white" : "link link-black"} href="#projects">Projects</a>
-                <a className={solidBackground ? "link link-white" : "link link-black"} href="#contact">Contact</a>
+            <div className="burger-menu" onClick={updateBurger}>
+                <div className={clickedBurger ? "burger-line line-1 burger-black" : "burger-line burger-black"}></div>
+                <div className={clickedBurger ? "burger-line line-2 burger-black" : "burger-line burger-black"}></div>
+                <div className={clickedBurger ? "burger-line line-3 burger-black" : "burger-line burger-black"}></div>
+            </div>
+            <div className={clickedBurger ? "nav-links show-menu" : "nav-links"}>
+                <a className={solidBackground && !clickedBurger ? "link link-white" : "link link-black"} href="#about">About</a>
+                <a className={solidBackground && !clickedBurger ? "link link-white" : "link link-black"} href="#projects">Projects</a>
+                <a className={solidBackground && !clickedBurger ? "link link-white" : "link link-black"} href="#contact">Contact</a>
             </div>
         </div>
     )
