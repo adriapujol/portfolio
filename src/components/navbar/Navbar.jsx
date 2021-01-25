cc80068f3b8a37f3c9e4e6ef6114272947b716db
Adria Pujol <adriapujolaparicio@gmail.com>
2021-01-25 09:36:15 +0100

    navbar working, no animation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,15 +20,24 @@ const Navbar = () => {
 
     window.addEventListener('scroll', changeBackground);
 
+
+    let whiteBurger1 = clickedBurger ? "burger-line line-1 burger-white" : "burger-line burger-white";
+    let whiteBurger2 = clickedBurger ? "burger-line line-2 burger-white" : "burger-line burger-white";
+    let whiteBurger3 = clickedBurger ? "burger-line line-3 burger-white" : "burger-line burger-white";
+    let blackBurger1 = clickedBurger ? "burger-line line-1 burger-black" : "burger-line burger-black";
+    let blackBurger2 = clickedBurger ? "burger-line line-2 burger-black" : "burger-line burger-black";
+    let blackBurger3 = clickedBurger ? "burger-line line-3 burger-black" : "burger-line burger-black";
+
+
     return (
         <div className={solidBackground ? "navbar bg-solid" : "navbar"}>
             <div className="logo">
                 <a className="home-link" href="#home">LOGO</a>
             </div>
             <div className="burger-menu" onClick={updateBurger}>
-                <div className={clickedBurger ? "burger-line line-1 burger-black" : "burger-line burger-black"}></div>
-                <div className={clickedBurger ? "burger-line line-2 burger-black" : "burger-line burger-black"}></div>
-                <div className={clickedBurger ? "burger-line line-3 burger-black" : "burger-line burger-black"}></div>
+                <div className={solidBackground && !clickedBurger ? whiteBurger1 : blackBurger1}></div>
+                <div className={solidBackground && !clickedBurger ? whiteBurger2 : blackBurger2}></div>
+                <div className={solidBackground && !clickedBurger ? whiteBurger3 : blackBurger3}></div>
             </div>
             <div className={clickedBurger ? "nav-links show-menu" : "nav-links"}>
                 <a className={solidBackground && !clickedBurger ? "link link-white" : "link link-black"} href="#about">About</a>
